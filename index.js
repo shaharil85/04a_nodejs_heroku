3565e0d8400ceada6da14df146aca2f6e63df33f
unknown <shaharil85@gmail.com>
2021-07-31 01:44:50 +0800

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 let app = require('express')();
 let server = require('http').createServer(app);
-let io = require('socket.io')(server);
+//let io = require('socket.io')(server);
+const io = require("socket.io")(server, {
+    cors: {
+        // origin: "https://localhost:8100/home",
+        // methods: ["GET", "POST"],
+        //allowedHeaders: ["my-custom-header"],
+        //credentials: true,
+        //origin: "http://localhost:8100",
+        //methods: ["GET", "POST"]
+        
+    }
+    
+});
   
 app.get('/', (req, res) => {
     res.status(200).send('Hello Yeah!!');
